9784914d9c4a7e26c929d387420503df248286c9
jjcp99 <48418411+jjcp99@users.noreply.github.com>
2019-05-27 01:24:27 +0200

    Add files via upload

--- Técnico/IISSI/js/modal.js
+++ Técnico/IISSI/js/modal.js
@@ -1,15 +1,36 @@
 var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
+var modal2 = document.getElementById('myModal2');
+var btn2 = document.getElementById("myBtn2");
 var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close2")[0];
+var bol = "<?php echo $_SESSION['errormodal']; ?>";
+
 
 btn.onclick = function() {
   modal.style.display = "block";  
 }
 span.onclick = function() {
   modal.style.display = "none";
+
 }
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target == modal ||event.target == modal2 ) {
     modal.style.display = "none";
+    modal2.style.display = "none";
   }
 }
+window.onload = function(event){
+  if(bol=="TRUE") {
+    modal.style.display = "block";
+
+  }
+}
+btn2.onclick = function() {
+  modal2.style.display = "block";  
+}
+
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
