3e177709304748bacaefc07b762dc08a871f05d7
Alberto Naranjo <48420916+AlbertoNR98@users.noreply.github.com>
2019-05-27 18:16:17 +0200

    Corección modal

--- ZeUS-WEB/Final/js/modal.js
+++ ZeUS-WEB/Final/js/modal.js
@@ -1,7 +1,5 @@
 var modal = document.getElementById('myModal');
-var modal2 = document.getElementById('myModal2');
 var btn = document.getElementById("myBtn");
-var consultabtn = document.getElementById("consultaBtn");
 var span = document.getElementsByClassName("close")[0];
 var bol = "<?php echo $_SESSION['errormodal']; ?>";
 
@@ -10,26 +8,17 @@ btn.onclick = function() {
   modal.style.display = "block";  
 }
 
-consultaBtn.onclick = function() {
-  modal2.style.display = "block";  
-}
-
 span.onclick = function() {
   modal.style.display = "none";
-  modal2.style.display = "none";
 }
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-  if (event.target == modal2) {
-    modal2.style.display = "none";
-  }
 }
 window.onload = function(event){
   if(bol=="TRUE") {
     modal.style.display = "block";
-    modal2.style.display ="block";
   }
 }
