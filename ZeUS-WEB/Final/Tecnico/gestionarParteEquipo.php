2d0d7fde097020c74a9a88d26a564ff8e9174241
jjcp99 <48418411+jjcp99@users.noreply.github.com>
2019-05-28 01:29:23 +0200

    Add files via upload

--- ZeUS-WEB/Final/Tecnico/gestionarParteEquipo.php
+++ ZeUS-WEB/Final/Tecnico/gestionarParteEquipo.php
@@ -1,4 +1,5 @@
 <?php
+
   /*
      * #===========================================================#
      * #	Este fichero contiene las funciones de gestión     			 
@@ -16,26 +17,77 @@ function quitar_parteEquipo($conexion,$PEID) { //hay q hacer procedimientos para
     }
 }
 
-function modificar_parteEquipo($conexion,$EID,$PEID,$REFERENCIA) {
+function modificar_parteEquipo($conexion,$EID,$PEID) {
 	try {
-		$stmt=$conexion->prepare("CALL MODIFICAR_PARTEEQUIPO(:EID,:PEID,:REFERENCIA)");
+		$stmt=$conexion->prepare("CALL MODIFICAR_PARTEEQUIPO(:EID,:PEID)");
 		$stmt->bindParam(':EID',$EID);
 		$stmt->bindParam(':PEID',$PEID);
-		$stmt->bindParam(':REFERENCIA',$REFERENCIA);
 		$stmt->execute();
 		return "";
 	} catch(PDOException $e) {
 		return $e->getMessage();
     }
 }
-function crear_parteEquipo($conexion,$EID,$REFERENCIA) { //hay q hacer procedimientos para esto
+function crear_parteEquipo($conexion,$EID) { //hay q hacer procedimientos para esto
 	try {
-		$stmt=$conexion->prepare("CALL crear_parteEquipo(:EID,:REFERENCIA)");
-        $stmt->bindParam(':EID',$EID);
-        $stmt->bindParam(':REFERENCIA',$REFERENCIA);
+		$stmt=$conexion->prepare("CALL CREAR_PARTEEQUIPO(:EID)");
+    $stmt->bindParam(':EID',$EID);
 		$stmt->execute();
 		return "";
 	} catch(PDOException $e) {
+		return $e->getMessage();
+		}
+		
+}
+function consultar_parteEquipo($conexion,$EID,$PEID){
+	try{
+		$consultaE="SELECT * FROM EVENTO WHERE eid=($EID)";
+		$stmt=$conexion->query($consultaE);
+		$resultadoEE=$stmt->fetch();
+		$consulta['evento']=$resultadoEE;
+
+		$consultaA="SELECT * FROM ALOJAMIENTO WHERE EID=($EID)";
+		$stmt=$conexion->query($consultaA);
+		$resultadoA=$stmt->fetch();
+		$consulta['alojamiento']=$resultadoA;
+		
+		$consultaT="SELECT * FROM TRANSPORTE WHERE EID=($EID)";
+		$stmt=$conexion->query($consultaT);
+		$resultadoT=$stmt->fetch();
+		$consulta['transporte']=$resultadoT;
+
+	 $_SESSION['Consulta']= $consulta;
+	 $_SESSION['peid']=$PEID;
+	
+		return "";
+	}catch(PDOException $e){
+		return $e->getMessage();
+	}
+}
+function listarIA($conexion,$PEID){
+	try{
+		$consulta = "SELECT * FROM ITEMALQUILADO WHERE IA IN (SELECT PEID FROM PARTEEQUIPO WHERE PEID=($PEID))"; 
+    	$stmt = $conexion->query($consulta);
+		return $stmt;
+	}catch(PDOException $e) {
+		return $e->getMessage();
+    }
+}
+function listarP($conexion,$PEID){
+	try{
+		$consulta = "SELECT * FROM PERSONAL WHERE PEID IN (SELECT PEID FROM PARTEEQUIPO WHERE PEID=($PEID))"; 
+    	$stmt = $conexion->query($consulta);
+		return $stmt;
+	}catch(PDOException $e) {
+		return $e->getMessage();
+    }
+}
+function listarI($conexion,$PEID){
+	try{
+		$consulta = "SELECT * FROM INVENTARIO WHERE PEID IN (SELECT PEID FROM PARTEEQUIPO WHERE PEID=($PEID))"; 
+    	$stmt = $conexion->query($consulta);
+		return $stmt;
+	}catch(PDOException $e) {
 		return $e->getMessage();
     }
 }
