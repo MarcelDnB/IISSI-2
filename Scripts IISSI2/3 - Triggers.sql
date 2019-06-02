58f48fea662be9e608b789ca05e97e5bdecfecbe
Alberto Naranjo <48420916+AlbertoNR98@users.noreply.github.com>
2019-06-02 13:14:08 +0200

    Actualización de los triggers de almacén

--- Scripts IISSI2/3 - Triggers.sql
+++ Scripts IISSI2/3 - Triggers.sql
@@ -1,14 +1,6 @@
 ----------------------------------Triggers--------------------------------------
---Inserta el �tem en la lista de reparaciones a la espera de un empleado que lo repare
-CREATE OR REPLACE TRIGGER mantenimiento1 
-BEFORE UPDATE ON INVENTARIO 
-FOR EACH ROW
-BEGIN
-  IF(:NEW.ESTADOITEM='porReparar') then insert into mantenimiento VALUES(SYSDATE,null, :OLD.REFERENCIA);
-  end if;
-END;
 
---Da el evento por finalizado al recibir de vuelta el env�o
+--Da el evento por finalizado al recibir de vuelta el envío
 CREATE OR REPLACE TRIGGER envios1 
 AFTER UPDATE ON envios 
 FOR EACH ROW
@@ -21,16 +13,19 @@ BEGIN
   end if;
 END;
 
---Pone el estado del �tem 'Disponible' cuendo finaliza el evento. 
-/*CREATE OR REPLACE TRIGGER EVENTO_inventario
-BEFORE UPDATE ON EVENTO 
+--Pone el estado del ítem 'Disponible' cuendo finaliza el evento. 
+CREATE OR REPLACE TRIGGER EVENTO_inventario
+AFTER UPDATE OF ESTADOEVENTO ON EVENTO 
 FOR EACH ROW
 declare aux integer;
 BEGIN
-select peid into aux from parteequipo where eid=:new.eid; 
-IF(:NEW.ESTADOEVENTO='realizado') then update inventario set estadoitem='Disponible' where peid=aux;
+select peid into aux from parteequipo where eid=:old.eid; 
+IF(:NEW.ESTADOEVENTO='Realizado') then
+update inventario set estadoitem='Disponible' where peid=aux;
+update inventario set PEID=null where peid=aux;
 end if;
 END;
+
 
 --Deja disponible al personal tras finalizar un evento. 
 CREATE OR REPLACE TRIGGER EVENTO_PERSONAL 
@@ -41,15 +36,8 @@ BEGIN
   update personal set estado='Libre' where eid=:new.eid;
   end if;
 END;
-*/
---Regla de negocio ARN01
-create or replace TRIGGER ARN01
-BEFORE INSERT OR UPDATE OF PID ON Inventario FOR EACH ROW
-BEGIN
-IF :new.PID > 999999 then
- raise_application_error(-20610, 'ID de usuario incorrecto');
-end if;
-end;
+
+--Regla de negocio ARN01 -> Se cumple sin necesidad de trigger
 
 --Regla de negocio ARN02
 CREATE OR REPLACE TRIGGER ARN02
@@ -60,16 +48,167 @@ IF :new.PID > 999999 then
 end if;
 end;
 
---Regla de negocio ARN04. ESTA REGLA SE HA HECHO TAMBI�N CON UN UNIQUE.
+--Regla de negocio ARN04. ESTA REGLA SE HA HECHO TAMBIÉN CON UN UNIQUE.
 CREATE OR REPLACE TRIGGER ARN04
 BEFORE INSERT ON PARTEEQUIPO 
 FOR EACH ROW
 DECLARE AUX INTEGER;
 BEGIN
 SELECT COUNT(*) INTO AUX FROM ENVIOS WHERE :NEW.PEID=PEID; 
-  IF(AUX=1) THEN RAISE_APPLICATION_ERROR(-20611,'Un envio no puede tener m�s de un parte de equipo asignado');
+  IF(AUX=1) THEN RAISE_APPLICATION_ERROR(-20611,'Un envio no puede tener más de un parte de equipo asignado');
   end if;
 END;
+
+--Trigger almacén IISSI2 1 Mira si el trabajador está libre
+CREATE OR REPLACE TRIGGER ARN05
+BEFORE INSERT OR UPDATE OF PID ON MANTENIMIENTO FOR EACH ROW
+DECLARE AUX INTEGER;
+BEGIN
+SELECT COUNT(*) INTO AUX FROM PERSONAL WHERE ESTADO='Ocupado' AND :NEW.PID = PID;
+ IF(AUX>0) THEN RAISE_APPLICATION_ERROR(-20630,'El trabajador ya está ocupado');
+ end if;
+ end;
+ 
+ --Trigger almacén IISSI2 2
+CREATE OR REPLACE TRIGGER ARN06
+BEFORE UPDATE OF PID ON MANTENIMIENTO FOR EACH ROW
+DECLARE AUX INTEGER;
+BEGIN
+SELECT COUNT(*) INTO AUX FROM PERSONAL WHERE DEPARTAMENTO='Almacen' AND :NEW.PID = PID;
+ IF(AUX=0) THEN RAISE_APPLICATION_ERROR(-20631,'El trabajador no es departamento de almacén');
+ end if;
+ end;
+ 
+ --Trigger almacén IISSI2 3 -> MIRA SI EL ÍTEM SELECCIONADO REQUIERE REPARACIÓN (ESTADO = PORREPARAR)
+CREATE OR REPLACE TRIGGER ARN07
+BEFORE UPDATE OF ESTADOITEM ON INVENTARIO FOR EACH ROW
+BEGIN
+  IF :NEW.ESTADOITEM='enMantenimiento' AND :OLD.ESTADOITEM!='porReparar' then
+   RAISE_APPLICATION_ERROR(-20632, 'El ítem no necesita reparación');
+end if;
+END;
+ 
+--Trigger almacén IISSI2 3 -> NO DEJA MANDAR A REPARACIONES PENDIENTES UN ÍTEM QUE YA SE ESTÁ REPARANDO
+CREATE OR REPLACE TRIGGER ARN08
+BEFORE UPDATE OF ESTADOITEM ON INVENTARIO FOR EACH ROW
+BEGIN
+  IF :NEW.ESTADOITEM='porReparar' AND :OLD.ESTADOITEM='enMantenimiento' then
+   RAISE_APPLICATION_ERROR(-20633, 'El ítem está en reparación');
+end if;
+END;
+
+--Trigger almacén IISSI2 4 -> NO DEJA REPARAR UN ÍTEM QUE NO EXISTE O QUE NO LO NECESITA -> Mantenimiento
+CREATE OR REPLACE TRIGGER ARN09
+BEFORE INSERT ON MANTENIMIENTO FOR EACH ROW
+DECLARE AUX INTEGER;
+BEGIN
+SELECT COUNT(*) INTO AUX FROM INVENTARIO WHERE REFERENCIA=:NEW.REFERENCIA AND ESTADOITEM='porReparar';
+ IF(AUX=0) THEN RAISE_APPLICATION_ERROR(-20634,'No se puede reparar el ítem');
+ end if;
+ end;
+
+--Trigger IISSI2 5 -> Mira si el ítem que se va a reparar lo necesita -> Inventario
+ CREATE OR REPLACE TRIGGER ARN13
+BEFORE UPDATE OF estadoItem ON INVENTARIO FOR EACH ROW
+BEGIN
+ IF :NEW.estadoItem='enMantenimiento' AND :OLD.estadoItem!='porReparar' THEN RAISE_APPLICATION_ERROR(-20637,'No se puede reparar el ítem');
+ end if;
+ end;
+ 
+--Trigger IISSI2 6 -> Actualiza el estado del personal si se le asigna una reparación -> SOLUCIÓN ALTERNATIVA AL PROCEDURE PARA ASIGNAR LA REPARACIÓN
+ CREATE OR REPLACE TRIGGER ARN14
+AFTER UPDATE OF PID ON MANTENIMIENTO FOR EACH ROW
+BEGIN
+UPDATE personal set estado='Ocupado' where PID=:NEW.PID;
+ end;
+ 
+--Trigger almacén IISSI2 7 -> Mira si el trabajador está ocupado antes de asignarlo a un envío
+CREATE OR REPLACE TRIGGER ARN10
+BEFORE UPDATE OF PID ON ENVIOS FOR EACH ROW
+DECLARE AUX INTEGER;
+BEGIN
+SELECT COUNT(*) INTO AUX FROM PERSONAL WHERE ESTADO='Ocupado' AND :NEW.PID = PID;
+ IF(AUX>0) THEN RAISE_APPLICATION_ERROR(-20630,'El trabajador ya está ocupado');
+ end if;
+ end;
+ 
+ --Trigger almacén IISSI2 8 -> Mira si el parte de equipo se esta enviando ya
+ CREATE OR REPLACE TRIGGER ARN11
+ BEFORE INSERT ON ENVIOS FOR EACH ROW
+ DECLARE AUX INTEGER;
+ BEGIN
+ SELECT COUNT(*) INTO AUX FROM ENVIOS WHERE PEID=:NEW.PEID;
+ IF(AUX>0) THEN RAISE_APPLICATION_ERROR(-20635,'Parte ya enviado');
+ END IF;
+ END;
+ 
+ --Trigger almacén IISSI2 9 -> Prohibe editar un envío ya recibido
+ CREATE OR REPLACE TRIGGER ARN12
+ BEFORE UPDATE ON ENVIOS FOR EACH ROW
+ BEGIN
+ IF :OLD.ESTADOENVIO='recibido' then RAISE_APPLICATION_ERROR(-20636, 'No se puede editar');
+end if;
+end;
+
+--Trigger almacén IISSI2 10 -> Mira si el empleado es de almacén antes de asignarlo a un envío
+CREATE OR REPLACE TRIGGER ARN15
+BEFORE INSERT ON ENVIOS FOR EACH ROW
+DECLARE AUX INTEGER;
+BEGIN
+SELECT COUNT(*) INTO AUX FROM PERSONAL WHERE DEPARTAMENTO='Almacen' AND :NEW.PID = PID;
+ IF(AUX=0) THEN RAISE_APPLICATION_ERROR(-20631,'El trabajador no es departamento de almacén');
+ end if;
+ end;
+
+--Trigger almacén IISSI2 11 -> Cambia el estado del ítem alquilado tras devolverlo
+CREATE OR REPLACE TRIGGER ARN16
+AFTER UPDATE OF ESTADODEVOLUCION ON DEVOLUCIONES
+FOR EACH ROW
+declare aux integer;
+BEGIN
+select ia into aux from devoluciones where did=:old.did; 
+IF(:NEW.ESTADODEVOLUCION='Devuelto') then
+update itemAlquilado set estado='Devuelto' where ia=aux;
+end if;
+END;
+
+--Trigger almacén IISSI2 12 -> Mira si el empleado está libre
+CREATE OR REPLACE TRIGGER ARN17
+BEFORE INSERT OR UPDATE OF PID ON DEVOLUCIONES FOR EACH ROW
+DECLARE AUX INTEGER;
+BEGIN
+SELECT COUNT(*) INTO AUX FROM PERSONAL WHERE ESTADO='Ocupado' AND :NEW.PID = PID;
+ IF(AUX>0) THEN RAISE_APPLICATION_ERROR(-20630,'El trabajador ya está ocupado');
+ end if;
+ end;
+ 
+ --Trigger almacén IISSI2 13 -> Mira si el empleado es de almacén
+CREATE OR REPLACE TRIGGER ARN18
+BEFORE INSERT OR UPDATE OF PID ON DEVOLUCIONES FOR EACH ROW
+DECLARE AUX INTEGER;
+BEGIN
+SELECT COUNT(*) INTO AUX FROM PERSONAL WHERE DEPARTAMENTO='Almacen' AND :NEW.PID = PID;
+ IF(AUX=0) THEN RAISE_APPLICATION_ERROR(-20631,'El trabajador no es departamento de almacén');
+ end if;
+ end;
+ 
+--Trigger almacén IISSI2 14  Mira si el ítem se ha devuelto ya
+ CREATE OR REPLACE TRIGGER ARN19
+ BEFORE INSERT ON DEVOLUCIONES FOR EACH ROW
+ DECLARE AUX INTEGER;
+ BEGIN
+ SELECT COUNT(*) INTO AUX FROM ITEMALQUILADO WHERE ESTADO='Devuelto';
+ IF(AUX>0) THEN RAISE_APPLICATION_ERROR(-20635,'Ítem ya devuelto');
+ END IF;
+ END;
+ 
+ --Trigger almacén IISSI2 15 -> Prohibe editar una devolución ya realizada
+ CREATE OR REPLACE TRIGGER ARN20
+ BEFORE UPDATE ON DEVOLUCIONES FOR EACH ROW
+ BEGIN
+ IF :OLD.ESTADODEVOLUCION='Devuelto' then RAISE_APPLICATION_ERROR(-20636, 'No se puede editar');
+end if;
+end;
 
 --Regla de negocio PRN01.1
 create or replace TRIGGER PRN011
@@ -95,8 +234,8 @@ FOR EACH ROW
 declare aux integer;
 BEGIN
     select count(eid) into aux from personal where eid=:new.eid;
-  IF((:NEW.ESTADOEVENTO='enPreparacion' and :old.estadoevento='porRealizar') and aux=0) then raise_application_error(-20614,'El evento no puede pasar a preparaci�n debido a que no hay ning�n 
-  t�cnico asociado al evento');
+  IF((:NEW.ESTADOEVENTO='enPreparacion' and :old.estadoevento='porRealizar') and aux=0) then raise_application_error(-20614,'El evento no puede pasar a preparación debido a que no hay ningún 
+  técnico asociado al evento');
   end if;
 END;
 
@@ -107,7 +246,7 @@ FOR EACH ROW
 declare aux integer;
 BEGIN
     select max(sueldo) into aux from personal where departamento!='Externo';
-  IF((:NEW.DEPARTAMENTO='Externo') AND :NEW.sueldo>2*aux) then raise_application_error(-20615,'El personal externo no puede tener m�s del doble del salario m�ximo del personal interno');
+  IF((:NEW.DEPARTAMENTO='Externo') AND :NEW.sueldo>2*aux) then raise_application_error(-20615,'El personal externo no puede tener más del doble del salario máximo del personal interno');
 end if;
 END;
 
@@ -120,7 +259,7 @@ eventofi evento.fechainicio%type;
 eventoff evento.fechafin%type;
 BEGIN
     select fechainicio into eventofi from evento where eid=:new.eid;
-  if(:new.fechainicio>=eventofi and :new.fechainicio<=eventoff) then raise_application_error(-20616,'Debe haber un margen de al menos 1 d�a en los extremos de la duraci�n del evento para contratar un alojamiento');
+  if(:new.fechainicio>=eventofi and :new.fechainicio<=eventoff) then raise_application_error(-20616,'Debe haber un margen de al menos 1 día en los extremos de la duración del evento para contratar un alojamiento');
   end if;
 END;
 
@@ -153,7 +292,7 @@ select count(*) into auxP from personal where (estado='Libre' and departamento='
 select count(*) into auxA from personal where (estado='Libre' and departamento='Almacen');
 select count(*) into auxT from personal where (estado='Libre' and departamento='Tecnico');
 select count(*) into auxC from personal where (estado='Libre' and departamento='Comercial');
-  IF(auxP<=2 or auxA<=2 or auxT<=2 or auxC<=2) then DBMS_OUTPUT.PUT_LINE('�ATENCION! Hay dos o menos trabajadores disponibles en alguno de los departamentos');
+  IF(auxP<=2 or auxA<=2 or auxT<=2 or auxC<=2) then DBMS_OUTPUT.PUT_LINE('¡ATENCION! Hay dos o menos trabajadores disponibles en alguno de los departamentos');
   end if;
 END;
 
@@ -167,7 +306,7 @@ aux1 integer;
 BEGIN
     select eid into aux1 from parteequipo where peid=:new.peid;
     select fechainicio into aux from evento where eid=aux1;
-  IF(:NEW.FECHALLEGADA>aux) then raise_application_error(-20618,'La fecha de llegada del �tem alquilado no puede ser posterior al inicio del evento');
+  IF(:NEW.FECHALLEGADA>aux) then raise_application_error(-20618,'La fecha de llegada del ítem alquilado no puede ser posterior al inicio del evento');
   end if;
 END;
 
@@ -178,7 +317,7 @@ declare aux integer; aux1 integer;
 BEGIN
   select count(*) into aux from personal where (estado='Ocupado' and departamento='Externo');
   select count(*) into aux1 from personal where (estado='Ocupado' and (departamento='Almacen' or departamento='Produccion' or departamento='Comercial' or departamento='Tecnico'));
-  if(aux>aux1) then DBMS_OUTPUT.PUT_LINE('ATENCION! Hay m�s trabajadores externos asociados a eventos, que trabajadores internos');
+  if(aux>aux1) then DBMS_OUTPUT.PUT_LINE('ATENCION! Hay más trabajadores externos asociados a eventos, que trabajadores internos');
   end if;
 END;
 
@@ -222,7 +361,7 @@ BEFORE UPDATE ON PERSONAL
 FOR EACH ROW
 BEGIN
 IF(:new.estado='Libre' AND :new.eid<>0) THEN
-RAISE_APPLICATION_ERROR(-20623,'No es posible que un empleado est� libre con un Evento asignado');
+RAISE_APPLICATION_ERROR(-20623,'No es posible que un empleado esté libre con un Evento asignado');
 END IF;
 END;
 
@@ -232,7 +371,7 @@ BEFORE UPDATE ON PERSONAL
 FOR EACH ROW
 BEGIN
 IF(:new.estado='Ocupado' AND :new.eid=0) THEN
-RAISE_APPLICATION_ERROR(-20624,'No es posible que un empleado est� ocupado sin ning�n Evento asignado');
+RAISE_APPLICATION_ERROR(-20624,'No es posible que un empleado esté ocupado sin ningún Evento asignado');
 END IF;
 END;
 
